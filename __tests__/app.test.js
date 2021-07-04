d11e88f93e5fd7a5981e8ea77b49c8ce4bfb6d76
Sean <simpson.sean@outlook.com>
2021-07-03 19:33:53 -0700

    working weather render

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,16 +28,23 @@ describe('app routes', () => {
     test('returns weather data', async() => {
 
       const expectation = [
-
-        {
-          "forecast": "Partly cloudy until afternoon.",
-          "time": "Tuesday, June 29, 2021"
-        },
-        {
-          "forecast": "Mostly cloudy in the morning.",
-          "time": "Wednesday, June 30, 2021"
-        },
-
+        {forecast: "Overcast clouds", time: "Thursday, July 01, 2021"},    
+        { forcast: 'Overcast clouds', time: 'Thursday, July 1, 2021' },
+        { forcast: 'Scattered clouds', time: 'Friday, July 2, 2021' },
+        { forcast: 'Clear Sky', time: 'Saturday, July 3, 2021' },
+        { forcast: 'Broken clouds', time: 'Sunday, July 4, 2021' },
+        { forcast: 'Scattered clouds', time: 'Monday, July 5, 2021' },
+        { forcast: 'Clear Sky', time: 'Tuesday, July 6, 2021' },
+        { forcast: 'Clear Sky', time: 'Wednesday, July 7, 2021' },
+        { forcast: 'Few clouds', time: 'Thursday, July 8, 2021' },
+        { forcast: 'Broken clouds', time: 'Friday, July 9, 2021' },
+        { forcast: 'Clear Sky', time: 'Saturday, July 10, 2021' },
+        { forcast: 'Clear Sky', time: 'Sunday, July 11, 2021' },
+        { forcast: 'Clear Sky', time: 'Monday, July 12, 2021' },
+        { forcast: 'Clear Sky', time: 'Tuesday, July 13, 2021' },
+        { forcast: 'Clear Sky', time: 'Wednesday, July 14, 2021' },
+        { forcast: 'Broken clouds', time: 'Thursday, July 15, 2021' },
+        { forcast: 'Broken clouds', time: 'Friday, July 16, 2021' },
       ];
       
       const data = await fakeRequest(app)
